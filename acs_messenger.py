e9a60b762494ae0003308a9b528b69abf54671f4
Juan Estrella <juan.estrella@airgas.com>
2025-03-04 17:09:11 -0500

    Validated SMS & Email working Added environment variables to gitignore

--- acs_messenger.py
+++ acs_messenger.py
@@ -7,37 +7,37 @@ import sendgrid
 from twilio.rest import Client
 from sendgrid.helpers.mail import *
 
+my_twilio_phone_number = "+18333655808"
 
-# Use environment variables later
-# sendgrid_api_key = os.environ.get("SENDGRID_API_KEY")
-# account_sid = os.environ.get("TWILIO_ACCOUNT_SID") # Replace with your Account SID
-# auth_token = os.environ.get("TWILIO_CLIENT_AUTH_TOKEN") # Replace with your auth token
+# source ./sendgrid.env
+sendgrid_api_key = os.environ.get("SENDGRID_API_KEY")
+sg = sendgrid.SendGridAPIClient(sendgrid_api_key) # Sendgrid email client
 
-
-sendgrid_api_key = "xxxxx"
-account_sid = "sdfghj" # Replace with your Account SID
-auth_token = "ertyuio" # Replace with your auth token
-
+# source ./sendgrid.env
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_CLIENT_AUTH_TOKEN")
 sms_client = Client(account_sid, auth_token) # Twilio sms client
-sg = sendgrid.SendGridAPIClient(sendgrid_api_key) # Twilio sendgrid email client
+
 
 db_params = {
     "dbname": "scadabase",
-    "user": "adminx",
+    "user": "postgres",
     "password": "hydrogen",
     "host": "127.0.0.1",
     "port": "5432"
 }
-conn = None
+conn = psycopg2.connect(**db_params, cursor_factory = DictCursor)
+conn.autocommit = True
 
 def main():
     try:
-        conn = psycopg2.connect(**db_params, cursor_factory = DictCursor)
         records = fetch_records()
         for record in records:
             result = process_record(record)
-            if not result: continue
-            archive_record(**record)
+            if not result:
+                print("Skipping")
+                continue
+            archive_record(record)
     except psycopg2.Error as e:
         print(f"Database Error: {e}")
     finally:
@@ -65,13 +65,14 @@ def fetch_records():
         with conn.cursor() as cur:
             cur.execute(sql)
             rows = cur.fetchall()
+            cur.close()
     except psycopg2.Error as e:
         print(f"Database error: {e}")
         conn.close()
     return rows
 
 
-def process_record(**record):
+def process_record(record):
     destination = record["DestinationAddress"]
     target = destination.strip().split('@')[0]
     if len(target) < 1:
@@ -86,34 +87,51 @@ def process_record(**record):
     return result
 
 
-def send_sms(**data):
+def send_sms(record):
     try:
-        target_phone_number = data["destination"].strip().split('@')[0]
+        target_phone_number = record["DestinationAddress"].strip().split('@')[0]
         message = sms_client.messages.create(
             to = target_phone_number,  # Replace with the recipient"s phone number
-            from_ = data["source"],  # Replace with your Twilio phone number
-            body = data["message"],
+            from_ = my_twilio_phone_number,  # Replace with your Twilio phone number
+            body = record["Body"],
         )
-        print(message.sid)
     except Exception as e:
         print(f"Error sending sms: {e}")
         return None
     return True
 
 
-def send_email(**data):
+def send_email(record):
     try:
-        from_email = Email(data["source"])
-        to_email = To(data["destination"])
-        subject = data["subject"]
-        content = Content("text/plain", data["message"])
-        mail = Mail(from_email, to_email, subject, content)
+        mail = Mail(
+            from_email = record["SourceAddress"],
+            subject = record["Subject"],
+            html_content = record["Body"]
+        )
+
+        personalization = Personalization()
+        personalization.add_to(To(record["DestinationAddress"]))
+
+        if record["CC_Address"] != None and len(record["CC_Address"].strip().split(',')) > 0:
+            list = record["CC_Address"].strip().split(',')
+            for cc in list:
+                val = cc.strip()
+                if len(val) < 6: continue # a@b.me -> anything shorter is prob bogus
+                personalization.add_cc(Cc(val))
+
+        if record["BCC_Address"] != None and len(record["BCC_Address"].strip().split(',')) > 0:
+            list = record["BCC_Address"].strip().split(',')
+            for bcc in list:
+                val = bcc.strip()
+                if len(val) < 6: continue # a@b.me -> anything shorter is prob bogus
+                personalization.add_bcc(Bcc(val))
+
+        mail.add_personalization(personalization)
         mail_json = mail.get()
         response = sg.client.mail.send.post(request_body = mail_json)
         print(response.status_code)
-        print(response.body)
-        print(response.headers)
-        if response.status_code != 200:
+        if response.status_code < 200 or response.status_code > 204:
+            print("Request failed")
             return False
     except Exception as e:
         print(f"Error sending email: {e}")
@@ -121,7 +139,7 @@ def send_email(**data):
     return True
 
 
-def archive_record(**record):
+def archive_record(record):
     id = record["ID"]
     source = record["SourceAddress"]
     destination = record["DestinationAddress"]
@@ -133,7 +151,7 @@ def archive_record(**record):
 
     try:
         with conn.cursor() as cur:
-            sql = 'DELETE FROM mail."MailQueue" WHERE "ID" = %s;'
+            sql = 'DELETE FROM mail."MailQueue" WHERE "ID" = %s'
             params = (id,)
             cur.execute(sql,params)
 
@@ -153,7 +171,7 @@ def archive_record(**record):
 def running_process_check():
     pid = os.getpid()
     for process in psutil.process_iter(["pid","name"]):
-        if process.info("name") == __file__ and process.info["pid"] != pid:
+        if process.info["name"] == __file__ and process.info["pid"] != pid:
             print(f"{__file__} is already running.")
             return False
     return True
